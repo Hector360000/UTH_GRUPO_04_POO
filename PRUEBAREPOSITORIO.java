597a0854ab871f3a59339c31d0282b94c076dbc1
Josue360000 <hectorjosue360000@gmail.com>
2024-11-05 21:52:17 -0600

    Update PRUEBAREPOSITORIO.java

--- PRUEBAREPOSITORIO.java
+++ PRUEBAREPOSITORIO.java
@@ -12,6 +12,6 @@ package com.mycompany.pruebarepositorio;
 public class PRUEBAREPOSITORIO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World55!");
     }
 }
